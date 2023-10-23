3f6318ac4f937af327734e58d9ab4d63a0e2ce0d
Gustaxz <guxtavogusteible@gmail.com>
2023-10-23 14:36:01 -0300

    começando o Root Directory Entry

--- src/main.go
+++ src/main.go
@@ -5,7 +5,8 @@ import (
 	"os"
 
 	bootSector "github.com/gustaxz/unifs/src/boot-sector"
-	FAT "github.com/gustaxz/unifs/src/fat"
+	directoryEntry "github.com/gustaxz/unifs/src/directory-entry"
+	"github.com/gustaxz/unifs/utils"
 )
 
 func check(e error) {
@@ -21,7 +22,7 @@ func main() {
 	// check(err)
 	// defer f.Close()
 
-	// err = handleDriver.CreateBootSector(f)
+	// err = bootSector.CreateBootSector(f)
 	// check(err)
 
 	f, err := os.Open("mydriver")
@@ -35,13 +36,33 @@ func main() {
 	check(err)
 	defer f.Close()
 
-	err = FAT.EntryAdressSectorAtFAT([]byte{0x10, 0xCC}, 0, f, &bootSector)
+	// err = FAT.EntryAdressSectorAtFAT([]byte{0x10, 0xCC}, 0, f, &bootSector)
+	// check(err)
+	// err = FAT.EntryAdressSectorAtFAT([]byte{0xDD, 0x1A}, 1, f, &bootSector)
+	// check(err)
+	// err = FAT.EntryAdressSectorAtFAT([]byte{0xBB, 0x98}, 2, f, &bootSector)
+	// check(err)
+	// emptyAdress, err := FAT.ListOfEmptyAdressesFAT(f, &bootSector)
+	// check(err)
+	// fmt.Println(emptyAdress)
+
+	data := directoryEntry.DirectoryEntry{
+		FileName:            [8]byte(utils.StringToBytes("GUSTAVO", 8)),
+		FileExtension:       [3]byte(utils.StringToBytes("TXT", 3)),
+		FileAttributes:      0x00,
+		Reserved:            [2]byte{0x00, 0x00},
+		CreationTime:        [2]byte{0x00, 0x00},
+		CreationDate:        [2]byte{0x00, 0x00},
+		LastAccessDate:      [2]byte{0x00, 0x00},
+		Ignored:             [2]byte{0x00, 0x00},
+		LastWriteTime:       [2]byte{0x00, 0x00},
+		LastWriteDate:       [2]byte{0x00, 0x00},
+		FirstLogicalCluster: [2]byte{0x00, 0x00},
+		FileSize:            [4]byte{0x00, 0x00, 0x00, 0x00},
+	}
+
+	err = directoryEntry.CreateDirectoryEntry(data, f, &bootSector)
 	check(err)
-	err = FAT.EntryAdressSectorAtFAT([]byte{0xDD, 0x1A}, 1, f, &bootSector)
+	err = directoryEntry.CreateDirectoryEntry(data, f, &bootSector)
 	check(err)
-	err = FAT.EntryAdressSectorAtFAT([]byte{0xBB, 0x98}, 2, f, &bootSector)
-	check(err)
-	emptyAdress, err := FAT.ListOfEmptyAdressesFAT(f, &bootSector)
-	check(err)
-	fmt.Println(emptyAdress)
 }
