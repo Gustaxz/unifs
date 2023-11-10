901066241a753a30a1ccda2765f7ffdb8df3857c
Gustaxz <guxtavogusteible@gmail.com>
2023-11-10 11:25:27 -0300

    adicionando função especial para criar uma root entry

--- src/directory-entry/root/create-root-directory-entry.go
+++ src/directory-entry/root/create-root-directory-entry.go
@@ -9,6 +9,17 @@ import (
 	"github.com/gustaxz/unifs/src/utils"
 )
 
+func CreateWithPosition(entry directoryEntry.DirectoryEntry, f *os.File, bootSector *bootSector.BootSectorMainInfos, position int) error {
+	sizeOfSector := bootSector.BytesPerSector
+	sizeOfFat := bootSector.SectorsPerFat * sizeOfSector
+	directoryEntrySize := 32
+
+	f.Seek(int64(sizeOfFat+sizeOfSector)+int64(directoryEntrySize)*int64(position), 0)
+	defer f.Seek(0, 0)
+
+	return binary.Write(f, binary.LittleEndian, utils.EncodeToBytes(entry))
+}
+
 func Create(entry directoryEntry.DirectoryEntry, f *os.File, bootSector *bootSector.BootSectorMainInfos) error {
 	freePositionsAtRootDirectoryEntry, err := FreePositions(f, bootSector)
 
