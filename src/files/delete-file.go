3409c0fd6deff70b3841d764a5a3ab83365c0529
Gustaxz <guxtavogusteible@gmail.com>
2023-11-10 11:03:57 -0300

    adicionando função para deletar arquivos

---
+++ src/files/delete-file.go
@@ -0,0 +1,44 @@
+package files
+
+import (
+	"os"
+
+	"github.com/gustaxz/unifs/src/blocks"
+	bootSector "github.com/gustaxz/unifs/src/boot-sector"
+	rootDirectoryEntry "github.com/gustaxz/unifs/src/directory-entry/root"
+	FAT "github.com/gustaxz/unifs/src/fat"
+)
+
+func DeleteFile(file File, f *os.File, bootSector *bootSector.BootSectorMainInfos) error {
+	fileFullName := make([]byte, 11)
+	copy(fileFullName, file.Name[:])
+	copy(fileFullName[8:], file.Ext[:])
+	infos, err := rootDirectoryEntry.FindFile(fileFullName, f, bootSector)
+	if err != nil {
+		return err
+	}
+
+	// TODO - Delete file from root directory entry
+
+	sectors, err := FAT.LinkedAdressesFAT(int(infos.FirstSector), f, bootSector)
+	if err != nil {
+		return err
+	}
+
+	//Deleting file from FAT
+	for _, sector := range sectors {
+		err = FAT.EntryAdressSectorAtFAT([]byte{0, 0}, sector, f, bootSector)
+		if err != nil {
+			return err
+		}
+
+	}
+
+	//Deleting file from Data Region
+	err = blocks.CreateSector(sectors, make([]byte, bootSector.BytesPerSector), f, bootSector)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
